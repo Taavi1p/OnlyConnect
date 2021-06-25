8a95d0a0790202acbfb30b120baac5718821e8cb
uku20 <73695231+uku20@users.noreply.github.com>
2021-06-25 19:12:43 +0300

    Data lisamine

---
+++ WeMakingMoves/data/info.js
@@ -0,0 +1,14 @@
+export default [
+    [
+        'Joogid',
+        [ 'Piim', 'Vesi', 'Viin', 'Õunamahl' ]
+    ],
+    [
+        'Söögid',
+        [ 'Vorst', 'Õun', 'Kartulisalat', 'Sašlõkk' ]
+    ],
+    [
+        'Mööbel',
+        [ 'Kapp', 'Voodi', 'Lamp', 'Laud' ]
+    ]
+]
