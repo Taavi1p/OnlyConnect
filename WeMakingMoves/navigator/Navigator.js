ca39dcebe871f71831ea045ecf2523b155103bfa
Taavi1p <Taavi1p@gmail.com>
2021-06-25 16:41:42 +0300

    navigaator

---
+++ WeMakingMoves/navigator/Navigator.js
@@ -0,0 +1,24 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import StartScreen from '../screens/StartScreen';
+import GameScreen from '../screens/GameScreen';
+
+const Navigator = createStackNavigator({
+    Start:{
+        screen: StartScreen,
+        navigationOptions: () => ({
+            header: null,
+          }),
+    },
+
+    Game: {
+        screen: GameScreen,
+        navigationOptions: () => ({
+            header: null,
+          }),
+    },
+}
+})
+
+export default createAppContainer(Navigator);
---
+++ WeMakingMoves/screens/StartScreen.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, ImageBackground} from 'react-native';
+
+//addime start screen nupu siia
+
+const StartScreen = () => {
+    return (
+        <View style={{ flex: 1}}>
+        <Text>Start screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    screen:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: 30
+    },
+    text: {
+        fontSize: 30,
+        color: 'white'
+    },
+    
+})
+
+export default StartScreen;
