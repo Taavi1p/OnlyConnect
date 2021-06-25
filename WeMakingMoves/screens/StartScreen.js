09fe2e418002c789aff8b8b7218b5da7c574ba59
uku20 <73695231+uku20@users.noreply.github.com>
2021-06-25 17:42:13 +0300

    screen

--- WeMakingMoves/screens/StartScreen.js
+++ WeMakingMoves/screens/StartScreen.js
@@ -1,28 +1,79 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, ImageBackground} from 'react-native';
 
-//addime start screen nupu siia
+import BeginButton from '../components/BeginButton';
 
-const StartScreen = () => {
+const StartScreen = props => {
+    
+    const startGame = () => {
+        props.navigation.navigate({routeName: 'Game'})
+    }
     return (
-        <View style={{ flex: 1}}>
-        <Text>Start screen</Text>
-        </View>
+                <View style={styles.container}>
+                    <View style={styles.buttons}>
+                        <BeginButton onClick={startGame}>START</BeginButton>
+                    </View>
+                </View>
     )
 }
 
 const styles = StyleSheet.create({
-    screen:{
+
+    image: {
         flex: 1,
-        justifyContent: 'center',
+        height: '100%',
+        width: '100%',
+    },
+    container: {
+        flex: 1,
+        marginTop: '20%',
+        paddingHorizontal: 40
+    },
+    title: {
+        fontSize: 70,
+        color: 'black',
+        fontFamily: 'avenir-heavy',
+        textAlign: 'center',
+        marginBottom: 30,
+    },
+    containerBox: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        justifyContent: "space-between",
         alignItems: 'center',
-        paddingVertical: 30
+    },
+    number: {
+        fontSize: 30,
     },
     text: {
         fontSize: 30,
-        color: 'white'
     },
-    
+    input: {
+        height: 30,
+        width: 60,
+        backgroundColor: 'black',
+    },
+    buttons: {
+        flexDirection: 'row',
+        marginTop: 'auto',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    emptyBox: {
+        width: '100%',
+        height: 35,
+    },
+    errorBox: {
+        backgroundColor: 'grey',
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+        marginTop: 20,
+    },
+    errorText: {
+        fontSize: 25,
+        color: 'white',
+        textAlign: 'center',
+    }
 })
 
 export default StartScreen;
